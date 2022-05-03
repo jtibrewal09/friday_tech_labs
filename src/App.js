adb772de9fb829372cf7b8ec1cd4c641c6f2de21
Jay Tibrewal <jtibrewal009@gmail.com>
2022-05-03 21:24:32 +0530

    added font library

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-import './App.scss';
 import { Button } from './@components';
+import { ReactComponent as Logo } from './assets/images/logo.svg';
+
+import './App.scss';
 
 function App() {
   return (
     <div className='page-container'>
       <header className='header'>
-        <img src='./images/logo.svg' alt='logo' />
-        <section>
-          <div></div>
-          <div></div>
-          <div></div>
+        <Logo />
+        <section className='menu-items-container'>
+          <div>About</div>
+          <div>Join Us</div>
+          <div>Portfolio</div>
         </section>
         <Button label="Hire Us" />
       </header>
+      <div className='body-tagline'>Your Idea.<br />
+        Confidence Led.<br />
+        Passion Built.</div>
     </div>
   );
 }
